5925dae31fc675df00f743fb29d2e68e95622411
박병욱 <bbwwpark@woowahan.com>
2023-05-23 21:07:02 +0900

    DB 연결

---
+++ src/main/java/hello/jdbc/connection/DBConnectionUtil.java
@@ -0,0 +1,26 @@
+package hello.jdbc.connection;
+
+import static hello.jdbc.connection.ConnectionConst.PASSWORD;
+import static hello.jdbc.connection.ConnectionConst.URL;
+import static hello.jdbc.connection.ConnectionConst.USERNAME;
+import lombok.extern.slf4j.Slf4j;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+@Slf4j
+public class DBConnectionUtil {
+
+    public static Connection getConnection() {
+        try {
+            //Connection 인터페이스(DB 드라이버들의 추상화)
+            //class org.h2.jdbc.JdbcConnection <- DriverManager를 통해 h2 DB드라이버 구현체를 가져옴
+            Connection connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+            log.info("get connection={}, class={}", connection, connection.getClass());
+            return connection;
+        } catch (SQLException e) {
+            throw new IllegalStateException(e);
+        }
+    }
+}
